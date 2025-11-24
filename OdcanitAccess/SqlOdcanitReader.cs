a6dbc012c8e8a48d47cad6bfd62241c7ae9fe965
dsarussi <dor.sarussi@gmail.com>
2025-11-24 10:48:22 +0200

    Add Monday phone/email mapping + MondaySettings integration

--- OdcanitAccess/SqlOdcanitReader.cs
+++ OdcanitAccess/SqlOdcanitReader.cs
@@ -45,22 +45,37 @@ namespace Odmon.Worker.OdcanitAccess
                 .Distinct()
                 .ToList();
 
-            var clients = await _db.Clients
+            var sideCounterSet = new HashSet<int>(sideCounters);
+            var visualIdSet = new HashSet<string>(visualIds);
+
+            var clientsFromDb = await _db.Clients
                 .AsNoTracking()
-                .Where(x => sideCounters.Contains(x.SideCounter) && visualIds.Contains(x.VisualID))
                 .ToListAsync(ct);
+
+            var clients = clientsFromDb
+                .Where(x => sideCounterSet.Contains(x.SideCounter) && visualIdSet.Contains(x.VisualID))
+                .ToList();
+
+            var clientLookup = clients
+                .GroupBy(x => new { x.SideCounter, x.VisualID })
+                .ToDictionary(
+                    g => (g.Key.SideCounter, g.Key.VisualID),
+                    g => g.First());
 
             foreach (var odcanitCase in cases)
             {
-                var client = clients.FirstOrDefault(x =>
-                    x.SideCounter == odcanitCase.SideCounter &&
-                    x.VisualID == odcanitCase.ClientVisualID);
-
-                if (client != null)
+                if (odcanitCase.ClientVisualID is null)
                 {
-                    odcanitCase.ClientPhone = client.Mobile;
-                    odcanitCase.ClientEmail = client.Email;
+                    continue;
                 }
+
+                if (!clientLookup.TryGetValue((odcanitCase.SideCounter, odcanitCase.ClientVisualID), out var client))
+                {
+                    continue;
+                }
+
+                odcanitCase.ClientPhone = client.Mobile;
+                odcanitCase.ClientEmail = client.Email;
             }
 
             return cases;
