b732b90abf63c57d2868c1f487b7a8434f2fec41
dsarussi <dor.sarussi@gmail.com>
2025-12-05 11:33:25 +0200

    Errors Fixed

--- Models/OdcanitHozlapMainData.cs
+++ Models/OdcanitHozlapMainData.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Odmon.Worker.Models
 {
     public class OdcanitHozlapMainData
     {
+        [Column("Counter")]
         public int TikCounter { get; set; }
         public string? clcCourtNum { get; set; }
         public string? CourtName { get; set; }
