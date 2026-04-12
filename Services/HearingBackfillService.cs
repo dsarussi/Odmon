75d29827c3810683a7768e1bb59ec273afdacda7
dsarussi <dor.sarussi@gmail.com>
2026-04-12 17:37:51 +0300

    May hearings added

--- Services/HearingBackfillService.cs
+++ Services/HearingBackfillService.cs
@@ -73,7 +73,7 @@ namespace Odmon.Worker.Services
             // Use raw SQL for OFFSET/FETCH since we have no Id (table from config; identifiers validated)
             // All polymorphic columns projected as nvarchar so EF reads string properties without type-cast failures.
             var rawSql = $@"
-SELECT [תאריך דיון], CONVERT(NVARCHAR(32), [שעת דיון]) AS [שעת דיון], [שם שופט], [שם ביהמש], [עיר בית משפט], [טלפון נהג], [שם נהג], [מספר תיק], CONVERT(NVARCHAR(64), [מספר לקוח]) AS [מספר לקוח], [תאריך אירוע]
+SELECT [תאריך דיון], CONVERT(NVARCHAR(32), [שעת דיון]) AS [שעת דיון], [שם שופט], [עיר בית משפט], [טלפון נהג], [שם נהג], [מספר תיק], CONVERT(NVARCHAR(64), [מספר לקוח]) AS [מספר לקוח], [תאריך אירוע]
 FROM {fromQualified}
 ORDER BY [תאריך דיון], [שעת דיון], [מספר תיק]
 OFFSET {{0}} ROWS FETCH NEXT {{1}} ROWS ONLY";
